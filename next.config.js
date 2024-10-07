2bb6eca318912ff3bf015bbb1855cef550617603
askar <askar@ask-ar.net>
2024-10-07 05:37:57 +0300

    ds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'build', // قم بتغيير `distDir` إلى مسار مختلف مثل `build`
+    assetPrefix: './',
 };
 
 module.exports = nextConfig;
