c893a6bbe24e5222c5885dbc3708e2b23ddcd352
askar <askar@ask-ar.net>
2024-10-07 06:09:57 +0300

    ds

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logo.wine',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'svgrepo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'aggravatedyouth.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
