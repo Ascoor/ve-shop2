1019789f7157a92950885282aa48186348522ae9
askar <askar@ask-ar.net>
2024-10-07 06:17:35 +0300

    d

--- next.config.js
+++ next.config.js
@@ -2,30 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'logo.wine',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'svgrepo.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'aggravatedyouth.com',
-      },
-    ],
-  },
+
+  output: 'standalone',  // لتقليل الملفات الغير مستخدمة
 };
 
 module.exports = nextConfig;
