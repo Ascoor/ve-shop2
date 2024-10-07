e1db197c1058a32f0b7e1b846cb4d13839bbf3b5
askar <askar@ask-ar.net>
2024-10-07 05:54:52 +0300

    cx

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  distDir: 'build', // قم بتغيير `distDir` إلى مسار مختلف مثل `build`
-    assetPrefix: './',
 };
 
 module.exports = nextConfig;
