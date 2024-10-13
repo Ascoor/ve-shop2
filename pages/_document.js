fe01ae5dd037f4404548be825d8f22ba8db45459
Ascoor <94120646+Ascoor@users.noreply.github.com>
2024-10-13 08:22:34 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;700&display=swap" rel="stylesheet" />
-          <meta name="description" content="VE-Shop - أفضل المنتجات والعروض" />
+<meta name="description" content="اكتشف أفضل المنتجات والعروض في متجر VE." />
           <meta name="keywords" content="متجر, شراء, منتجات, تسوق, إلكترونيات, ملابس" />
         </Head>
         <body className="font-noto bg-gray-100">
