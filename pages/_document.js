2379fb60ea88cda5cbe882a53a317b22526e698e
Ascoor <94120646+Ascoor@users.noreply.github.com>
2024-10-13 08:21:53 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ar" dir="rtl">
         <Head>
+      <link rel="icon" href="/path/to/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@400;700&display=swap" rel="stylesheet" />
