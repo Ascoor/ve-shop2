9764411ab570e0dab56c25c019cd2d2fa4b08d9c
Ascoor <94120646+Ascoor@users.noreply.github.com>
2024-10-16 17:30:57 +0300

    Add files via upload

---
+++ pages/dashboard/products/manageCategories/index.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Managecategories = () => {
+  return (
+    <div>
+      <h1>Managecategories</h1>
+      <p>This is a demo content for the Managecategories page.</p>
+    </div>
+  );
+};
+
+export default Managecategories;
