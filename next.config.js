54de7eeb96fe424cf14f58ac998dda4bb5ac2289
Ascoor <94120646+Ascoor@users.noreply.github.com>
2024-10-20 02:20:25 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,47 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   
-  // Output as static files
+  // Output as static files for production
   output: 'export',
 
+  // Disable image optimization for static export
   images: {
-    unoptimized: true, // Disable image optimization since this is a static export
+    unoptimized: true,
   },
 
+  // Enable compression for better performance
   compress: true,
+
+  // Add security headers to improve security against common attacks
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline';", // Adjust the sources as needed
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
