f92d561851b59b4e4b73feedf119b93b5ae1e40f
askar <mr.askar1984@gmail.com>
2024-10-19 19:23:35 +0300

    يس

--- next.config.js
+++ next.config.js
@@ -1,50 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // تفعيل الوضع الصارم في React لعرض الأخطاء والتحذيرات
   reactStrictMode: true,
-  
-  // استخدام محرك التحسين SWC لضغط الشيفرة لتحسين الأداء
   swcMinify: true,
+  
+  // إعداد لنشر المشروع كـ static export
+  output: 'export',  // تصدير ملفات ثابتة
 
-  // إعداد لنشر المشروع كـ standalone
-  output: 'standalone',
-
-  // إعدادات الصور
   images: {
-    // السماح بجلب الصور من مضيف خارجي محدد
+    formats: ["image/jpg", "image/webp", "svg"],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 've-shop.co',
-        port: '', // عدم تحديد منفذ يعني استخدام المنفذ الافتراضي (443 للـ https)
-        pathname: '/**', // السماح بجلب أي صورة من هذا النطاق
+        port: '', 
+        pathname: '/**',
       },
     ],
-    // الأحجام المسموحة للأجهزة المختلفة
     deviceSizes: [640, 750, 1080, 1200, 1920],
-    // الأحجام المحددة للصور التي يمكن استخدامها
     imageSizes: [16, 32, 48, 64, 96],
-    // التنسيقات المسموح بها للصور
     formats: ['image/avif', 'image/webp'],
   },
 
-  // تفعيل الضغط لتحسين أداء التحميل
   compress: true,
 
-  // تخصيص إعدادات Webpack
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // في حال كان التطبيق يعمل على المتصفح (client-side)، استخدام Sentry Browser
       config.resolve.alias['@sentry/node'] = '@sentry/browser';
     }
     return config;
   },
 
-  // إعدادات ESLint لتحديد المجلدات التي سيتم فحصها للتأكد من خلوها من الأخطاء
   eslint: {
-    dirs: ['pages', 'components', 'lib'], // يمكنك إضافة أي مجلدات أخرى تحتاج إلى فحصها
+    dirs: ['pages', 'components', 'lib'],
   },
 
+  // إعدادات الترجمة الدولية
+  i18n: {
+    locales: ['en', 'ar'], 
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
