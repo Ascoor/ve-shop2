07b4f28857a8d06bd300d6fb6e966a9872528953
askar <mr.askar1984@gmail.com>
2024-10-19 20:27:40 +0300

    rt

--- next.config.js
+++ next.config.js
@@ -2,36 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  output: 'standalone',
+  
+  // Output as static files
+  output: 'export',
 
   images: {
- 
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 've-shop.co', 
-        port: '', 
-        pathname: '/**',
-      },
-    ],
-    deviceSizes: [640, 750, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96],
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true, // Disable image optimization since this is a static export
   },
 
   compress: true,
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias['@sentry/node'] = '@sentry/browser';
-    }
-    return config;
-  },
-
-  eslint: {
-    dirs: ['pages', 'components', 'lib'],
-  },
 };
 
 module.exports = nextConfig;
